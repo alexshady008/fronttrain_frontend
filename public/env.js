634942b10f4ebc5e76529d3dc9b72032baebbd37
alexshady008 <alexshady008.2@gmail.com>
2022-03-02 20:38:35 -0300

    Cambios de la api url

---
+++ public/env.js
@@ -0,0 +1,6 @@
+window.env = {
+  "URL_SERVER": "https://git.heroku.com/front-train-backend.git/api",
+  "URL_SERVER_DB": "https://git.heroku.com/front-train-backend.git/database/products/",
+  "URL_SERVER_USER_DB": "https://git.heroku.com/front-train-backend.git/database/avatars/",
+  "URL_SERVER_PDF": "https://git.heroku.com/front-train-backend.git/pdf/"
+};
--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,19 @@
-//require('dotenv').config()
+import env from 'react-dotenv'
+// import path from 'path'
+// import dotenv from 'dotenv'
+// dotenv.config({ path: path.join(__dirname, '../.env') });
+
+console.log(env)
 
 const config = {
-    urlServer: process.env.URL_SERVER || 'http://localhost:4000/api',
-    urlServerDb: process.env.URL_SERVER_DB || 'http://localhost:4000/database/products/',
-    urlServerUserDb: process.env.URL_SERVER_USER_DB || 'http://localhost:4000/database/avatars/',
-    urlServerPdf: process.env.URL_SERVER_PDF || 'http://localhost:4000/pdf/',
+    // urlServer: env.URL_SERVER || 'http://localhost:4000/api',
+    // urlServerDb: env.URL_SERVER_DB || 'http://localhost:4000/database/products/',
+    // urlServerUserDb: env.URL_SERVER_USER_DB || 'http://localhost:4000/database/avatars/',
+    // urlServerPdf: env.URL_SERVER_PDF || 'http://localhost:4000/pdf/',
+    urlServer: 'https://git.heroku.com/front-train-backend.git/api',
+    urlServerDb: 'https://git.heroku.com/front-train-backend.git/database/products/',
+    urlServerUserDb: 'https://git.heroku.com/front-train-backend.git/database/avatars/',
+    urlServerPdf: 'https://git.heroku.com/front-train-backend.git/pdf/',
     limitPage: 10,
     limitOffset: 100,
     intervalSesion: 1000,
